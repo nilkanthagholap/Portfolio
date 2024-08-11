56d1b42c767f59c0e6264fafa59579f7d971e6ce
Nilkanta Namdev Gholap <nilkanthagholap@gmail.com>
2024-08-11 14:02:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('.collapsible-header').forEach(header => {
+  header.addEventListener('click', () => {
+      const collapsibleContent = header.nextElementSibling;
+
+      header.parentElement.classList.toggle('active');
+
+      collapsibleContent.style.display = collapsibleContent.style.display === 'block' ? 'none' : 'block';
+  });
+});
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
