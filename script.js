87e7513a273ecc46db626d6f9c50e02621d48c9e
Nilkanta Namdev Gholap <nilkanthagholap@gmail.com>
2024-08-11 17:48:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,24 @@ const currentYear = new Date().getFullYear();
 document.getElementById('current-year').textContent = currentYear;
 
 
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
