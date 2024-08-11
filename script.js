ed13ea0a737f255d4c38918f31d3b8c1259340d9
Nilkanta Namdev Gholap <nilkanthagholap@gmail.com>
2024-08-11 17:32:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,4 +28,10 @@ function myFunction() {
   }
 }
 
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Set the current year in the footer
+document.getElementById('current-year').textContent = currentYear;</script>
+
 
