839c3827b61a66d8837e394d47f5018af83e7c4b
Nilkanta Namdev Gholap <nilkanthagholap@gmail.com>
2024-10-21 10:48:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
-  } else {
+  }); else {
     mybutton.style.display = "none";
   });
 });
