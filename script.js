ab0293358589f2fb3ae438790bcd7027009d0aa5
Nilkanta Namdev Gholap <nilkanthagholap@gmail.com>
2024-10-21 10:42:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,14 +55,6 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
 
-// Disable right click
-document.onkeydown = function (event) {
-	event = (event || window.event);
-	    if (event.keyCode == 123 || event.keyCode == 18)
-	{
-		return false;
-	}
-	}
-document.addEventListener('contextmenu', event => event.preventDefault());
+
 
 
