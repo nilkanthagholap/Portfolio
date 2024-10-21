a9295e4b3a5d7a39c351e3d6bce558b5a7951a68
Nilkanta Namdev Gholap <nilkanthagholap@gmail.com>
2024-10-21 10:16:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,4 +56,13 @@ function topFunction() {
 });
 
 
+		document.onkeydown = function (event) {
+		     event = (event || window.event);
+		     if (event.keyCode == 123 || event.keyCode == 18)
+		     {
+		           return false;
+		     }
+		}
+		document.addEventListener('contextmenu', event => event.preventDefault());
+
 
